217d741607ead6914a08577838452de2a6bf3b95
John yeager <johnyeager@Johns-MacBook-Pro.local>
2018-06-01 12:38:45 -0400

    got voice command ready

--- src/assets/voice.js
+++ src/assets/voice.js
@@ -51,7 +51,8 @@ function toggleStartStop() {
       for (let i = 0; i < e.results.length; ++i) {
         if (e.results[i].isFinal) {
           final += e.results[i][0].transcript;
-          console.log('final transcription:', e.results[i][0].transcript);
+          testSpeech(e.results[i][0].transcript);
+          getCommandString();
           chatTextArea.text(e.results[i][0].transcript);
           chatTextArea.focus();
           if (recognizing === true) {
@@ -68,6 +69,21 @@ function toggleStartStop() {
     interimTextDisplay.text('');
   }
 }
+
+
+
+function testSpeech(val){
+  document.getElementById('chatTextArea').innerHTML = val;
+}
+
+
+
+
+
+
+
+
+
 
 function startSpeech(){
   // transcriptDisplay.toggle();
@@ -143,6 +159,13 @@ function startSpeech(){
     chatSubmitHandler();
   });
 
+
+
+
+  function getCommandString(){
+    // console.log(document.getElementById('chatTextArea').innerHTML);
+    document.getElementById('chatTextArea').click();
+  }
 
 
 
