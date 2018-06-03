c70000a42699f961c14bb687cc8232b7eec5944b
Alexander <agonz519@gmail.com>
2018-06-03 18:20:50 -0400

    Fixed callCommand.js to get it working again

--- src/assets/callCommands.js
+++ src/assets/callCommands.js
@@ -1,84 +1,74 @@
+// Command Functions
+const takeOff = () => {
+  console.log('TAKE OFF!');
+  $.get('https://localhost:8002/api/takeoff', (data) => {});
+};
+
+const calibrate = () => {
+  console.log('CALIBRATE!');
+  $.get('https://localhost:8002/api/calibrate', (data) => {});
+};
+
+const moveLeft = () => {
+  console.log('MOVE LEFT!');
+  $.get('https://localhost:8002/api/move-left', (data) => {});
+};
+
+const moveRight = () => {
+  console.log('MOVE RIGHT!');
+  $.get('https://localhost:8002/api/move-right', (data) => {});
+};
+
+const moveUp = () => {
+  console.log('MOVE UP!');
+  $.get('https://localhost:8002/api/move-up', (data) => {});
+};
+
+const moveDown = () => {
+  console.log('MOVE DOWN!');
+  $.get('https://localhost:8002/api/move-down', (data) => {});
+};
+
+const moveFront = () => {
+  console.log('MOVE FRONT!');
+  $.get('https://localhost:8002/api/move-front', (data) => {});
+};
+
+const moveBack = () => {
+  console.log('MOVE BACK!');
+  $.get('https://localhost:8002/api/move-back', (data) => {});
+};
+
+const turnLeft = () => {
+  console.log('TURN LEFT!');
+  $.get('https://localhost:8002/api/turn-left', (data) => {});
+};
+
+const turnRight = () => {
+  console.log('TURN RIGHT!');
+  $.get('https://localhost:8002/api/turn-right', (data) => {});
+};
+
+const stop = () => {
+  console.log('STOP!');
+  $.get('https://localhost:8002/api/stop', (data) => {});
+};
+
+const land = () => {
+  console.log('LAND!');
+  $.get('https://localhost:8002/api/land', (data) => {});
+};
+
+console.log( "ready!" );
+
 $( document ).ready(function() {
-  console.log( "ready!" );
-
-  // TAKE OFF BUTTON
-  $('#takeOff').on('click', (event) => {
-    event.preventDefault();
-    takeOff();
-  });
-
-  // CALIBRATE BUTTON
-  $('#calibrate').on('click', (event) => {
-    event.preventDefault();
-    calibrate();
-  });
-
-  // MOVE LEFT BUTTON
-  $('#moveLeft').on('click', (event) => {
-    event.preventDefault();
-    moveLeft();
-  });
-
-  // MOVE RIGHT BUTTON
-  $('#moveRight').on('click', (event) => {
-    event.preventDefault();
-    moveRight();
-  });
-
-  // MOVE UP BUTTON
-  $('#moveUp').on('click', (event) => {
-    event.preventDefault();
-    moveUp();
-  });
-
-  // MOVE DOWN BUTTON
-  $('#moveDown').on('click', (event) => {
-    event.preventDefault();
-    moveDown();
-  });
-
-  // MOVE FRONT BUTTON
-  $('#moveFront').on('click', (event) => {
-    event.preventDefault();
-    moveFront();
-  });
-
-  // MOVE BACK BUTTON
-  $('#moveBack').on('click', (event) => {
-    event.preventDefault();
-    moveBack();
-  });
-
-  // TURN LEFT BUTTON
-  $('#turnLeft').on('click', (event) => {
-    event.preventDefault();
-    turnLeft();
-  });
-
-  // TURN RIGHT BUTTON
-  $('#turnRight').on('click', (event) => {
-    event.preventDefault();
-    turnRight();
-  });
-
-  // STOP BUTTON
-  $('#stop').on('click', (event) => {
-    event.preventDefault();
-    stop();
-  });
-
-  // LAND BUTTON
-  $('#land').on('click', (event) => {
-    event.preventDefault();
-    land();
-  });
 
   let chatTextAreaDisplay = document.getElementById('chatTextArea');
   let oldVal = chatTextAreaDisplay.innerHTML;
 
-  function checkChange(){
+  function checkChange() {
 
-    if(chatTextAreaDisplay.innerHTML !== oldVal) {
+    if (chatTextAreaDisplay.innerHTML !== oldVal) {
       console.log("value changed");
 
       if (chatTextAreaDisplay.innerHTML === 'Taking off!') {
@@ -111,68 +101,89 @@ $( document ).ready(function() {
       console.log('nope');
     }
   }
+
   setInterval(checkChange, 2000);
 
-  // Command Functions
-
-  const takeOff = () => {
-    console.log('TAKE OFF!');
-    $.get('https://localhost:8002/api/takeoff', (data) => {});
-  };
-
-  const calibrate = () => {
-    console.log('CALIBRATE!');
-    $.get('https://localhost:8002/api/calibrate', (data) => {});
-  };
-
-  const moveLeft = () => {
-    console.log('MOVE LEFT!');
-    $.get('https://localhost:8002/api/move-left', (data) => {});
-  };
-
-  const moveRight = () => {
-    console.log('MOVE RIGHT!');
-    $.get('https://localhost:8002/api/move-right', (data) => {});
-  };
-
-  const moveUp = () => {
-    console.log('MOVE UP!');
-    $.get('https://localhost:8002/api/move-up', (data) => {});
-  };
-
-  const moveDown = () => {
-    console.log('MOVE DOWN!');
-    $.get('https://localhost:8002/api/move-down', (data) => {});
-  };
-
-  const moveFront = () => {
-    console.log('MOVE FRONT!');
-    $.get('https://localhost:8002/api/move-front', (data) => {});
-  };
-
-  const moveBack = () => {
-    console.log('MOVE BACK!');
-    $.get('https://localhost:8002/api/move-back', (data) => {});
-  };
-
-  const turnLeft = () => {
-    console.log('TURN LEFT!');
-    $.get('https://localhost:8002/api/turn-left', (data) => {});
-  };
-
-  const turnRight = () => {
-    console.log('TURN RIGHT!');
-    $.get('https://localhost:8002/api/turn-right', (data) => {});
-  };
-
-  const stop = () => {
-    console.log('STOP!');
-    $.get('https://localhost:8002/api/stop', (data) => {});
-  };
-
-  const land = () => {
-    console.log('LAND!');
-    $.get('https://localhost:8002/api/land', (data) => {});
-  };
-
 });
+
+// TAKE OFF BUTTON
+$(document).on('click', '#takeOff', (event) => {
+  event.preventDefault();
+  takeOff();
+});
+
+// CALIBRATE BUTTON
+$(document).on('click', '#calibrate', (event) => {
+  event.preventDefault();
+  calibrate();
+});
+
+// MOVE LEFT BUTTON
+$(document).on('click', '#moveLeft', (event) => {
+  event.preventDefault();
+  moveLeft();
+});
+
+// MOVE RIGHT BUTTON
+$(document).on('click', '#moveRight', (event) => {
+  event.preventDefault();
+  moveRight();
+});
+
+// MOVE UP BUTTON
+$(document).on('click', '#moveUp', (event) => {
+  event.preventDefault();
+  moveUp();
+});
+
+// MOVE DOWN BUTTON
+$(document).on('click', '#moveDown', (event) => {
+  event.preventDefault();
+  moveDown();
+});
+
+// MOVE FRONT BUTTON
+$(document).on('click', '#moveFront', (event) => {
+  event.preventDefault();
+  moveFront();
+});
+
+// MOVE BACK BUTTON
+$(document).on('click', '#moveBack', (event) => {
+  event.preventDefault();
+  moveBack();
+});
+
+// TURN LEFT BUTTON
+$(document).on('click', '#turnLeft', (event) => {
+  event.preventDefault();
+  turnLeft();
+});
+
+// TURN RIGHT BUTTON
+$(document).on('click', '#turnRight', (event) => {
+  event.preventDefault();
+  turnRight();
+});
+
+// STOP BUTTON
+$(document).on('click', '#stop', (event) => {
+  event.preventDefault();
+  stop();
+});
+
+// LAND BUTTON
+$(document).on('click', '#land', (event) => {
+  event.preventDefault();
+  land();
+});
+
+
+
+
+
+
+
+
+
+
