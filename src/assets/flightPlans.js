07587815b7072f8d646dd204640fd50677cab218
Geoff's Macbook <geoffsmacbook@Geoffss-MBP.attlocal.net>
2018-06-04 21:14:42 -0400

    fixes button styling and backgound image issues

--- src/assets/flightPlans.js
+++ src/assets/flightPlans.js
@@ -25,28 +25,14 @@ let flightPlansModel = [
   }
 ];
 
-Array.prototype.unique = function() {
-  let arr = [];
-  for(let i = 0; i < this.length; i++) {
-    if(!arr.includes(this[i])) {
-      arr.push(this[i]);
-    }
-  }
-  return arr;
-};
+
 
 //Pulls unique list of flight plans ids
 let flightPlansIds = [];
 for (let i = 0; i < flightPlanActions.length; i++) {
-  flightPlansIds.push(flightPlanActions[i].flight_plan_id);
 }
 flightPlansIds = flightPlansIds.unique();
 
-//Pulls unique list of flight plans names
-let flightPlansNames = [];
-for (let i = 0; i < flightPlanActions.length; i++) {
-  flightPlansIds.push(flightPlanActions[i].flight_plan_name);
-}
-flightPlansNames = flightPlansNames.unique();
+
 
 
